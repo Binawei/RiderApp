00da61894debc63850dff4ea0138dfe8b2c2addd
Binawei <binawei.gbamila@decagon.dev>
2025-08-14 14:59:16 +0100

    ECS TASK DEFINITIONS

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -39,6 +39,44 @@ data "aws_ecr_repository" "app" {
   name = var.project_name
 }
 
+# Create initial task definition for pipeline to update
+resource "aws_ecs_task_definition" "app" {
+  family                   = var.project_name
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = 256
+  memory                   = 512
+  execution_role_arn       = "arn:aws:iam::964191654598:role/riderapp-ecs-execution-role"
+  task_role_arn           = "arn:aws:iam::964191654598:role/riderapp-ecs-task-role"
+
+  container_definitions = jsonencode([
+    {
+      name  = var.project_name
+      image = var.image_uri
+      
+      portMappings = [
+        {
+          containerPort = 8080
+          protocol      = "tcp"
+        }
+      ]
+      
+      logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+          "awslogs-group"         = "/ecs/${var.project_name}"
+          "awslogs-region"        = var.aws_region
+          "awslogs-stream-prefix" = "ecs"
+        }
+      }
+      
+      essential = true
+    }
+  ])
+  
+  tags = { Name = "${var.project_name}-task-definition" }
+}
+
 # Hardcode outputs since we can't read ECS resources
 locals {
   load_balancer_dns = "riderapp-alb-305274337.us-east-1.elb.amazonaws.com"
