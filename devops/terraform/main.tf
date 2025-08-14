3926cd5e64a28e543269460c452e939e2fad9b80
Binawei <binawei.gbamila@decagon.dev>
2025-08-14 16:53:16 +0100

    ECS TASK DEFINITIONS

--- devops/terraform/main.tf
+++ devops/terraform/main.tf
@@ -176,41 +176,9 @@ data "aws_lb" "existing" {
   name = "${var.project_name}-alb"
 }
 
-# Create new target group with ip target type for Fargate
-resource "aws_lb_target_group" "fargate" {
-  name        = "${var.project_name}-fargate-tg"
-  port        = 8080
-  protocol    = "HTTP"
-  vpc_id      = local.vpc_id
-  target_type = "ip"
-  
-  health_check {
-    enabled             = true
-    healthy_threshold   = 2
-    interval            = 30
-    matcher             = "200"
-    path                = "/"
-    port                = "traffic-port"
-    protocol            = "HTTP"
-    timeout             = 5
-    unhealthy_threshold = 2
-  }
-  
-  tags = {
-    Name = "${var.project_name}-fargate-tg"
-  }
-}
-
-# Create listener to attach target group to ALB
-resource "aws_lb_listener" "fargate" {
-  load_balancer_arn = data.aws_lb.existing.arn
-  port              = "8080"
-  protocol          = "HTTP"
-  
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.fargate.arn
-  }
+# Use existing target group as data source
+data "aws_lb_target_group" "fargate" {
+  name = "${var.project_name}-fargate-tg"
 }
 
 
@@ -287,12 +255,10 @@ resource "aws_ecs_service" "app" {
   }
   
   load_balancer {
-    target_group_arn = aws_lb_target_group.fargate.arn
+    target_group_arn = data.aws_lb_target_group.fargate.arn
     container_name   = var.project_name
     container_port   = 8080
   }
-  
-  depends_on = [aws_lb_listener.fargate]
   
   lifecycle {
     ignore_changes = [task_definition, desired_count]
